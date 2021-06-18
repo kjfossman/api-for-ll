51a0634e546c988b3131dd2096fc58d78fb1443d
Kyle <kjfossman@gmail.com>
2021-06-18 11:21:21 -0800

    added wins and losses to the teams models

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,7 +2,22 @@ class Team < ApplicationRecord
     has_many :home_games, :class_name => "Game", :foreign_key => 'home_team_id'
     has_many :away_games, :class_name => "Game", :foreign_key => 'away_team_id'
 
-    has_many :wins, :class_name => "Game", :foreign_key => 'winner'
-    has_many :losses, :class_name => "Game", :foreign_key => 'loser'
+    def wins
+        games = self.away_games + self.home_games
+
+        games.collect.count do |x|
+                x.winner == self.id 
+            end
+    end
+
+    def losses 
+        games = self.away_games + self.home_games
+
+        games.collect.count do |x|
+                x.loser == self.id 
+            end
+    end
+    # has_many :wins, :class_name => "Game", :foreign_key => 'winner'
+    # has_many :losses, :class_name => "Game", :foreign_key => 'loser'
 
 end
