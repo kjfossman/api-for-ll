e197606567ebcaa5535df1a4da365827e6ef6235
Kyle <kjfossman@gmail.com>
2021-07-09 15:54:23 -0800

    Added resource Contact

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_30_174836) do
+ActiveRecord::Schema.define(version: 2021_07_09_220437) do
 
   create_table "announcements", force: :cascade do |t|
     t.text "subject"
     t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "name"
+    t.string "phone"
+    t.string "email"
+    t.string "title"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
