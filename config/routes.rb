8eebea3a1815b8190afbcf661cc67bff1350ef98
Kyle <kjfossman@gmail.com>
2021-08-04 16:37:30 -0800

    Updating routes.rb so that they have the sessions and updating application controller to have helper methods related to user auth

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   resources :players
   resources :games
   resources :teams
-  resources :sessions
+  
+  post '/login', to: 'sessions#create'
+  post '/logout',   to: 'sessions#destroy'
+  get '/logged_in', to: 'sessions#is_logged_in?'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
