e87aa48049606497a2b6eeb153bde6d70b08b24c
Kyle <kjfossman@gmail.com>
2021-08-04 16:42:45 -0800

    Added to the sessions controller to help with user auth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,48 @@
 class SessionsController < ApplicationController
     
-  
-    # GET /users
-    def index
-        byebug
-      @sessions = Player.all
-  
-      render json: @sessions
+    def create
+        @user = User.find_by(username: session_params[:username])
+      
+        if @user && @user.authenticate(session_params[:password])
+          login!
+          render json: {
+            logged_in: true,
+            user: @user
+          }
+        else
+          render json: { 
+            status: 401,
+            errors: ['user does not exist, please try again']
+          }
+        end
+    end
+
+    def is_logged_in?
+        if logged_in? && current_user
+          render json: {
+            logged_in: true,
+            user: current_user
+          }
+        else
+          render json: {
+            logged_in: false,
+            message: 'no user is logged in'
+          }
+        end
+    end
+
+    def destroy
+          logout!
+          render json: {
+            status: 200,
+            logged_out: true
+          }
+    end
+
+    private
+
+    def session_params
+          params.require(:user).permit(:username, :password)
     end
 end
   
