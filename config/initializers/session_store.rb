a0775f5e855ffd36cc0da689085b75392e105c43
Kyle <kjfossman@gmail.com>
2021-08-14 14:45:16 -0800

    removed domain on session_store.rb

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env == 'production' 
-    Rails.application.config.session_store :cookie_store, key: '_little-league-api', domain: "little-league-api.herokuapp.com"
+    Rails.application.config.session_store :cookie_store, key: '_little-league-api'
   else
     Rails.application.config.session_store :cookie_store, key: '_api-for-ll'
   end
