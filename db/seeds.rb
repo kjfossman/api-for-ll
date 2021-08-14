c4c7801b49720f29acba6bc16d9cadd7c60ba79e
Kyle <kjfossman@gmail.com>
2021-08-14 07:58:08 -0800

    Added seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Team.create(name: "Oakland A's", ages: "12-14", img: "https://i.pinimg.com/originals/da/b8/5a/dab85a4b406c5619ef226b694054ba4b.png")
+Team.create(name: "San Francisco Giants", ages: "12-14", img: "https://i.pinimg.com/236x/08/7e/eb/087eeb046f3a452915df46c0d46ec029--giant-games-giants-baseball.jpg")
+Team.create(name: "Boston Red Sox", ages: "8-11", img: "https://i.pinimg.com/originals/59/e0/1b/59e01b8f0bdbe917a54a7493d8585776.jpg")
+Team.create(name: "New York Yankees", ages: "8-11", img: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/70/NewYorkYankees_caplogo.svg/1024px-NewYorkYankees_caplogo.svg.png")
+
