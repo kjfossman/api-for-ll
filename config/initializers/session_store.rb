75d1c19ad37a876ea5d058578f1700440fbef475
Kyle <kjfossman@gmail.com>
2021-08-14 12:12:36 -0800

    updated session store.rb for heroku

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
-if Rails.env === 'production' 
-    Rails.application.config.session_store :cookie_store, key: '_little-league-api', :domain => :all, tld_length: 2
+if Rails.env == 'production' 
+    Rails.application.config.session_store :cookie_store, key: '_little-league-api', domain: "little-league-api.herokuapp.com"
   else
     Rails.application.config.session_store :cookie_store, key: '_api-for-ll'
   end
